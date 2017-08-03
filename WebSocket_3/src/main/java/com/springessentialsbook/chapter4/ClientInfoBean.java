663817aa59732b0bf8e09554a860c584ea281515
Guilherme Bahia <gbvbahia01@gmail.com>
2017-08-03 19:25:49 -0300

    websocket app3

--- WebSocket_3/src/main/java/com/springessentialsbook/chapter4/ClientInfoBean.java
+++ WebSocket_3/src/main/java/com/springessentialsbook/chapter4/ClientInfoBean.java
@@ -1,9 +1,14 @@
 package com.springessentialsbook.chapter4;
 
 public class ClientInfoBean {
+
     private String clientName;
     private String clientMessage;
-    public String getClientMessage() { return clientMessage; }
+
+    public String getClientMessage() {
+        return clientMessage;
+    }
+
     public String getClientName() {
         return clientName;
     }
